24838909bbe4a616dd9191987bcaf81589daf6a4
Лагодюк Юрий <cafebabe@yandex-team.com.ua>
2013-01-04 01:46:53 +0200

    fix of complete link

--- src/main/java/com/lagodiuk/clustering/CompleteLink.java
+++ src/main/java/com/lagodiuk/clustering/CompleteLink.java
@@ -6,7 +6,7 @@ public class CompleteLink extends Hierarchical {
 
 	@Override
 	protected <T> double distance(TypedTreeNode<T> baseNode, TypedTreeNode<T> targetNode, Map<T, Map<T, Double>> distances) {
-		double dist = Double.MAX_VALUE;
+		double dist = -1;
 		for (T item1 : baseNode.breadthFirstItems()) {
 			for (T item2 : targetNode.breadthFirstItems()) {
 				dist = Math.max(distances.get(item1).get(item2), dist);
